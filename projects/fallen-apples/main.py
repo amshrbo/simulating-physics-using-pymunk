a25c6e40b18f147ec5e5c2e7c3c8499489574967
amshrbo <amshrbo@gmail.com>
2021-06-18 21:54:02 +0200

    A fallen apple collides with two static objects

--- projects/fallen-apples/main.py
+++ projects/fallen-apples/main.py
@@ -4,19 +4,19 @@ import sys
 
 # Initializing pygame
 pyg.init()
-window = pyg.display.set_mode((600, 600))
+window = pyg.display.set_mode((800, 800))
 clock = pyg.time.Clock()
 
 # Initializing pymunk
 space = pyk.Space()
-space.gravity = (0, 100) # adding horizontal and vertical gravity
+space.gravity = (0, 200) # adding horizontal and vertical gravity
 
 # Creating static circles
 def static_ball(space, pos):
     #specifing the type of the body and the shape of it
     body = pyk.Body(body_type = pyk.Body.STATIC)
     body.position = pos
-    shape = pyk.Circle(body, 50)    
+    shape = pyk.Circle(body, 20)    
 
     space.add(body, shape)
 
@@ -27,13 +27,37 @@ def draw_ball(balls):
         pos_x = int(ball.body.position.x)
         pos_y = int(ball.body.position.y)
 
-        pyg.draw.circle(window, (0,0,0), (pos_x, pos_y), 30)
+        pyg.draw.circle(window, (0,0,0), (pos_x, pos_y), 20)
 
 
 # Creating a list of balls
 balls = []
 balls.append(static_ball(space, (170, 300)))
 balls.append(static_ball(space, (300, 450)))
+
+
+# Creaing apples 
+def create_apple(space, pos):
+    body = pyk.Body(1, 1000, body_type = pyk.Body.DYNAMIC)
+    body.position = pos
+    shape = pyk.Circle(body, 20)
+
+    space.add(body, shape)
+
+    return shape
+
+# creating the apple image
+green_apple = pyg.image.load('assets/apple.svg')
+def draw_apple(apples):
+    for apple in apples:
+        pos_x = int(apple.body.position.x)
+        pos_y = int(apple.body.position.y)
+
+        apple_rect = green_apple.get_rect(center = (pos_x, pos_y))
+        window.blit(green_apple, apple_rect)
+
+apples = []
+apples.append(create_apple(space, (305, 0)))
 
 
 while True:
@@ -49,6 +73,7 @@ while True:
 
     # Drawing two static balls 
     draw_ball(balls)
+    draw_apple(apples)
 
     # Updating pymunk space inside pygame
     space.step(0.02)
